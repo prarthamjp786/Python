8d048d499fc306aca15bb5320717420e1bfbcff7
Prathmesh Pathak <34978373+prarthamjp786@users.noreply.github.com>
2020-06-23 15:38:44 -0400

    Added python movement on keypress

--- python.js
+++ python.js
@@ -1,17 +1,18 @@
+import { getInputDirection } from "./input.js";
+
 export const pythonSpeed = 2;
 const pythonBody = [
-    { x: 10, y: 11 },
-    { x: 11, y: 11 },
-    { x: 12, y: 11 }
+    { x: 11, y: 11 }
 ];
 
 export function update() {
+    const inputDirection = getInputDirection();
     for (let i = pythonBody.length - 2; i >= 0; i--) {
         pythonBody[i + 1] = { ...pythonBody[i] };
     }
 
-    pythonBody[0].x += 0;
-    pythonBody[0].y += 1;
+    pythonBody[0].x += inputDirection.x;
+    pythonBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
